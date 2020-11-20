6e8ab98670b974412ff59c6f6fd4885d427b2c9f
MartinHalus <halus.martin@gmail.com>
2020-11-20 23:36:41 +0100

    8:58 How to Crop an Image in Android (with ActivityResultContracts)

--- app/src/main/java/com/maha/imagercroppy/MainActivity.kt
+++ app/src/main/java/com/maha/imagercroppy/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.maha.imagercroppy
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.theartofdev.edmodo.cropper.CropImage
 
 class MainActivity : AppCompatActivity() {
+
+    private val cropActivityResultContract = object : ActivityResultContract<Any?, Uri?{
+        override fun createIntent(context: Context, input: Any?): Intent {
+            return CropImage.activity()
+                .setAspectRatio(16,9)
+                .getIntent(this@MainActivity)
+        }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+            return CropImage.getActivityResult(intent)?.uri
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ActivityResultContracts.
     }
+
+
+
 }
